62b197ed8b1c7bd27b859cb029d780407c93ca62
naikriya754-ship-it <naikriya754@gmail.com>
2026-05-11 12:58:13 +0530

    Add files via upload

---
+++ Write a function to format contact details nicely for display.py
@@ -0,0 +1,9 @@
+#Write a function to format contact details nicely for display.
+def format_contact(name,phone,email):
+    return f"""
+Name : {name}
+Phone : {phone}
+Email : {email}
+"""
+print(format_contact("riya naik","345678","riya24@gmail.com"))
+      
